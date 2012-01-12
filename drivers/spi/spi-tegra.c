8aaa905cae43bbbed04b3c271b81bcf003f1ba38
Ashwini Ghuge <aghuge@nvidia.com>
2012-01-12 10:50:44 +0530

    drivers: spi: tegra: Add runtime PM support
    Bug 886806
    Original commit: http://git-master/r/59905
    
    Change-Id: Ia8d64f3810d6157c6029180cd9a3cf98c2d3cb4c
    Signed-off-by: Ashwini Ghuge <aghuge@nvidia.com>
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    Reviewed-on: http://git-master/r/74896
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Varun Colbert <vcolbert@nvidia.com>
    Tested-by: Varun Colbert <vcolbert@nvidia.com>
    
    Conflicts:
    	drivers/spi/spi-tegra.c

--- drivers/spi/spi-tegra.c
+++ drivers/spi/spi-tegra.c
@@ -33,7 +33,8 @@
 #include <linux/interrupt.h>
 #include <linux/delay.h>
 #include <linux/completion.h>
-
+#include <linux/kthread.h>
+#include <linux/pm_runtime.h>
 #include <linux/spi/spi.h>
 #include <linux/spi-tegra.h>
 
@@ -689,7 +690,7 @@ static void spi_tegra_start_transfer(struct spi_device *spi,
 	if (is_first_of_msg) {
 		if (!tspi->is_clkon_always) {
 			if (!tspi->clk_state) {
-				clk_enable(tspi->clk);
+				pm_runtime_get_sync(&tspi->pdev->dev);
 				tspi->clk_state = 1;
 			}
 		}
@@ -845,16 +846,18 @@ static int spi_tegra_setup(struct spi_device *spi)
 	tspi->def_command_reg |= val;
 
 	if (!tspi->is_clkon_always && !tspi->clk_state) {
-		clk_enable(tspi->clk);
+		spin_unlock_irqrestore(&tspi->lock, flags);
+		pm_runtime_get_sync(&tspi->pdev->dev);
+		spin_lock_irqsave(&tspi->lock, flags);
 		tspi->clk_state = 1;
 	}
 	spi_tegra_writel(tspi, tspi->def_command_reg, SLINK_COMMAND);
 	if (!tspi->is_clkon_always && tspi->clk_state) {
-		clk_disable(tspi->clk);
 		tspi->clk_state = 0;
-	}
-
-	spin_unlock_irqrestore(&tspi->lock, flags);
+		spin_unlock_irqrestore(&tspi->lock, flags);
+		pm_runtime_put_sync(&tspi->pdev->dev);
+	} else
+		spin_unlock_irqrestore(&tspi->lock, flags);
 	return 0;
 }
 
@@ -949,7 +952,7 @@ static void spi_tegra_curr_transfer_complete(struct spi_tegra_data *tspi,
 					/* Provide delay to stablize the signal
 					   state */
 					udelay(10);
-					clk_disable(tspi->clk);
+					pm_runtime_put_sync(&tspi->pdev->dev);
 					tspi->clk_state = 0;
 				}
 			}
@@ -1303,7 +1306,8 @@ static int __init spi_tegra_probe(struct platform_device *pdev)
 	tspi->def_command2_reg = SLINK_CS_ACTIVE_BETWEEN;
 
 skip_dma_alloc:
-	clk_enable(tspi->clk);
+	pm_runtime_enable(&pdev->dev);
+	pm_runtime_get_sync(&pdev->dev);
 	tspi->clk_state = 1;
 	master->dev.of_node = pdev->dev.of_node;
 	ret = spi_register_master(master);
@@ -1335,7 +1339,7 @@ fail_rx_buf_alloc:
 	if (tspi->rx_dma)
 		tegra_dma_free_channel(tspi->rx_dma);
 fail_rx_dma_alloc:
-	clk_put(tspi->clk);
+	pm_runtime_disable(&pdev->dev);
 fail_clk_get:
 	free_irq(tspi->irq, tspi);
 fail_irq_req:
@@ -1369,10 +1373,11 @@ static int __devexit spi_tegra_remove(struct platform_device *pdev)
 		tegra_dma_free_channel(tspi->rx_dma);
 
 	if (tspi->is_clkon_always) {
-		clk_disable(tspi->clk);
+		pm_runtime_put_sync(&pdev->dev);
 		tspi->clk_state = 0;
 	}
 
+	pm_runtime_disable(&pdev->dev);
 	clk_put(tspi->clk);
 	iounmap(tspi->base);
 
@@ -1405,7 +1410,7 @@ static int spi_tegra_suspend(struct platform_device *pdev, pm_message_t state)
 
 	spin_unlock_irqrestore(&tspi->lock, flags);
 	if (tspi->is_clkon_always) {
-		clk_disable(tspi->clk);
+		pm_runtime_put_sync(&pdev->dev);
 		tspi->clk_state = 0;
 	}
 	return 0;
@@ -1420,12 +1425,11 @@ static int spi_tegra_resume(struct platform_device *pdev)
 	master = dev_get_drvdata(&pdev->dev);
 	tspi = spi_master_get_devdata(master);
 
-	spin_lock_irqsave(&tspi->lock, flags);
-	clk_enable(tspi->clk);
+	pm_runtime_get_sync(&pdev->dev);
 	tspi->clk_state = 1;
 	spi_tegra_writel(tspi, tspi->command_reg, SLINK_COMMAND);
 	if (!tspi->is_clkon_always) {
-		clk_disable(tspi->clk);
+		pm_runtime_put_sync(&pdev->dev);
 		tspi->clk_state = 0;
 	}
 
@@ -1434,6 +1438,37 @@ static int spi_tegra_resume(struct platform_device *pdev)
 	spin_unlock_irqrestore(&tspi->lock, flags);
 	return 0;
 }
+#endif
+
+#if defined(CONFIG_PM_RUNTIME)
+
+static int tegra_spi_runtime_idle(struct device *dev)
+{
+	struct spi_master       *master;
+	struct spi_tegra_data   *tspi;
+	master = dev_get_drvdata(dev);
+	tspi = spi_master_get_devdata(master);
+
+	clk_disable(tspi->clk);
+	return 0;
+}
+
+static int tegra_spi_runtime_resume(struct device *dev)
+{
+	struct spi_master       *master;
+	struct spi_tegra_data   *tspi;
+	master = dev_get_drvdata(dev);
+	tspi = spi_master_get_devdata(master);
+
+	clk_enable(tspi->clk);
+	return 0;
+}
+
+static const struct dev_pm_ops tegra_spi_dev_pm_ops = {
+	.runtime_idle = tegra_spi_runtime_idle,
+	.runtime_resume = tegra_spi_runtime_resume,
+};
+
 #endif
 
 MODULE_ALIAS("platform:spi_tegra");
@@ -1452,6 +1487,9 @@ static struct platform_driver spi_tegra_driver = {
 	.driver = {
 		.name =		"spi_tegra",
 		.owner =	THIS_MODULE,
+#if defined(CONFIG_PM_RUNTIME)
+		.pm =		&tegra_spi_dev_pm_ops,
+#endif
 		.of_match_table = spi_tegra_of_match_table,
 	},
 	.remove =	__devexit_p(spi_tegra_remove),
