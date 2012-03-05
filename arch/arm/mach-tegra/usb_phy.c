e93f39fa682bc4fdb8e3fb3d28ef73a0da553e02
Jubeom Kim <jubeomk@nvidia.com>
2012-03-05 15:00:45 +0900

    tegra: usb: phy: fix the struct name of hsic
    Change-Id: I54a46230ee743ac9c740048cdb38328947d52c3c
    Signed-off-by: Jubeom Kim <jubeomk@nvidia.com>
    Reviewed-on: http://git-master/r/96133
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- arch/arm/mach-tegra/usb_phy.c
+++ arch/arm/mach-tegra/usb_phy.c
@@ -2500,7 +2500,7 @@ struct tegra_usb_phy *tegra_usb_phy_open(int instance, void __iomem *regs,
 	int i;
 	int err;
 #ifndef CONFIG_ARCH_TEGRA_2x_SOC
-	struct tegra_ulpi_config *uhsic_config;
+	struct tegra_uhsic_config *uhsic_config;
 	int reset_gpio, enable_gpio;
 #endif
 
