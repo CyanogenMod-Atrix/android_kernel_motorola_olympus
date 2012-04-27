b21def416c5850657ea7a366b8eb5dbb3a562452
Vishal Singh <vissingh@nvidia.com>
2012-04-27 18:10:33 +0530

    mmc: enable background ops in driver
    Adding a new config option and enabling background ops in driver.
    Correcting the EXT_CSD byte that needs to be written in order to
    trigger background ops in the MMC firmware.
    
    Bug 847037.
    
    Change-Id: Ibc517540cab43fa5070b142a416f6b67f2f7e7be
    Signed-off-by: Vishal Singh <vissingh@nvidia.com>
    Reviewed-on: http://git-master/r/99117
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- drivers/mmc/host/sdhci-tegra.c
+++ drivers/mmc/host/sdhci-tegra.c
@@ -1122,6 +1122,11 @@ static int __devinit sdhci_tegra_probe(struct platform_device *pdev)
 		host->mmc->caps |= MMC_CAP_NONREMOVABLE;
 	}
 	host->mmc->pm_flags |= MMC_PM_IGNORE_PM_NOTIFY;
+
+#ifdef CONFIG_MMC_BKOPS
+	host->mmc->caps |= MMC_CAP_BKOPS;
+#endif
+
 #ifdef CONFIG_MMC_EMBEDDED_SDIO
 	/* Do not turn OFF embedded sdio cards as it support Wake on Wireless */
 	if (plat->mmc_data.embedded_sdio)
