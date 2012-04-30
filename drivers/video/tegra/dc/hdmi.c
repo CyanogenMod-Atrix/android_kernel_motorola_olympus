86c9754e625b6b9ac86db944c08158c895ca7919
Shashank Sharma <shashanks@nvidia.com>
2012-04-30 19:23:57 +0530

    video: tegra: fb: Call mode filter from check var
    Call dc_hdmi_mode_filter to validate a videomode. X prepares its
    own modedb of supported HDMI modes, but all of them may not be
    supported from the HDMI driver. This call makes sure a X-mode is
    listed only if supported in DC driver.
    
    Bug: 959676
    
    Change-Id: I8aff65f4e08fcc4471af096150e3972b5913a95a
    Signed-off-by: Shashank Sharma <shashanks@nvidia.com>
    Reviewed-on: http://git-master/r/99650
    Reviewed-by: Rohan Somvanshi <rsomvanshi@nvidia.com>
    Tested-by: Rohan Somvanshi <rsomvanshi@nvidia.com>

--- drivers/video/tegra/dc/hdmi.c
+++ drivers/video/tegra/dc/hdmi.c
@@ -1289,7 +1289,6 @@ static bool tegra_dc_hdmi_mode_filter(const struct tegra_dc *dc,
 	return false;
 }
 
-
 static bool tegra_dc_hdmi_hpd(struct tegra_dc *dc)
 {
 	return tegra_dc_hpd(dc);
@@ -2399,6 +2398,7 @@ struct tegra_dc_out_ops tegra_dc_hdmi_ops = {
 	.detect = tegra_dc_hdmi_detect,
 	.suspend = tegra_dc_hdmi_suspend,
 	.resume = tegra_dc_hdmi_resume,
+	.mode_filter = tegra_dc_hdmi_mode_filter,
 };
 
 struct tegra_dc_edid *tegra_dc_get_edid(struct tegra_dc *dc)
