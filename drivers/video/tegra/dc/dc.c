9c36a7d25b978864c25a5c5649cd803898872600
Hunk Lin <hulin@nvidia.com>
2012-05-03 05:35:47 +0800

    Revert "video: tegra: dc: Set default videomode"
    This reverts commit 9349cedf17f9b3c10760c8d48f831473f87a3a15.
    
    It is reviewed on http://git-master/r/99635
    It will cause HDMI power ON and emc clock bump up to 667Mhz
    after resume from LP0.
    
    bug 930136
    
    Change-Id: I130494fdb381b3d322ac0e3fc8be2e44f2c2d7a7
    Signed-off-by: Hunk Lin <hulin@nvidia.com>
    Reviewed-on: http://git-master/r/100202
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Jon Mayo <jmayo@nvidia.com>

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -64,20 +64,6 @@
 #endif
 
 static int no_vsync;
-static struct fb_videomode tegra_dc_hdmi_fallback_mode = {
-	.refresh = 60,
-	.xres = 640,
-	.yres = 480,
-	.pixclock = KHZ2PICOS(25200),
-	.hsync_len = 96,	/* h_sync_width */
-	.vsync_len = 2,		/* v_sync_width */
-	.left_margin = 48,	/* h_back_porch */
-	.upper_margin = 33,	/* v_back_porch */
-	.right_margin = 16,	/* h_front_porch */
-	.lower_margin = 10,	/* v_front_porch */
-	.vmode = 0,
-	.sync = 0,
-};
 
 static void _tegra_dc_controller_disable(struct tegra_dc *dc);
 
@@ -2636,19 +2622,10 @@ static bool _tegra_dc_controller_reset_enable(struct tegra_dc *dc)
 }
 #endif
 
-static int _tegra_dc_set_default_videomode(struct tegra_dc *dc)
-{
-	return tegra_dc_set_fb_mode(dc, &tegra_dc_hdmi_fallback_mode, 0);
-}
-
 static bool _tegra_dc_enable(struct tegra_dc *dc)
 {
-	if (dc->mode.pclk == 0) {
-		/*  DC enable called but no videomode is loaded.
-		     Set fallback mode to enable driver with.*/
-		if (_tegra_dc_set_default_videomode(dc))
-			return false;
-	}
+	if (dc->mode.pclk == 0)
+		return false;
 
 	if (!dc->out)
 		return false;
