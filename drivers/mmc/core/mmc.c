45db91063208a3f973ed0cf78a4a0a5b9f54a36d
Vishal Singh <vissingh@nvidia.com>
2012-05-18 19:03:11 +0530

    mmc: core: adding new sysfs entry for prod_rev
    Adding new sysfs entry for product revision which is needed by
    mNAND health and status user space tool.
    
    Bug 974823.
    
    Change-Id: I4a07a5e6c1c81e2b7a4c1ed024e987e06f9f87c8
    Signed-off-by: Vishal Singh <vissingh@nvidia.com>
    Reviewed-on: http://git-master/r/103366
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Sandeep Trasi <strasi@nvidia.com>
    Reviewed-by: Luis Dib <ldib@nvidia.com>
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>
    Reviewed-by: Pavan Kunapuli <pkunapuli@nvidia.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -522,6 +522,7 @@ MMC_DEV_ATTR(hwrev, "0x%x\n", card->cid.hwrev);
 MMC_DEV_ATTR(manfid, "0x%06x\n", card->cid.manfid);
 MMC_DEV_ATTR(name, "%s\n", card->cid.prod_name);
 MMC_DEV_ATTR(oemid, "0x%04x\n", card->cid.oemid);
+MMC_DEV_ATTR(prv, "0x%x\n", card->cid.prod_rev);
 MMC_DEV_ATTR(serial, "0x%08x\n", card->cid.serial);
 MMC_DEV_ATTR(enhanced_area_offset, "%llu\n",
 		card->ext_csd.enhanced_area_offset);
@@ -538,6 +539,7 @@ static struct attribute *mmc_std_attrs[] = {
 	&dev_attr_manfid.attr,
 	&dev_attr_name.attr,
 	&dev_attr_oemid.attr,
+	&dev_attr_prv.attr,
 	&dev_attr_serial.attr,
 	&dev_attr_enhanced_area_offset.attr,
 	&dev_attr_enhanced_area_size.attr,
