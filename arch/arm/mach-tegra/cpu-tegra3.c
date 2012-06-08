9d8eea6716367751c6f21134bc54fd91ab91a75a
Xiao Bo Zhao <xiaoboz@nvidia.com>
2012-06-07 17:11:14 -0700

    arm: tegra: modify hotplug balance threshold
    Tuned the sensitivity level of the hot-plug governor's balancing algorithm to 60% from 75%
    
    Bug 958978
    
    Change-Id: I972ed908995e348e8b3b0afb8108c44b66c80eae
    Signed-off-by: Wen Yi <wyi@nvidia.com>
    (cherry picked from commit 07a69db5af4c971b2c2d161ee149580ab5b40633)
    Reviewed-on: http://git-master/r/111641
    Reviewed-by: Sai Gurrappadi <sgurrappadi@nvidia.com>
    Tested-by: Sai Gurrappadi <sgurrappadi@nvidia.com>
    Reviewed-by: Peter Boonstoppel <pboonstoppel@nvidia.com>
    Reviewed-by: Yu-Huan Hsu <yhsu@nvidia.com>

--- arch/arm/mach-tegra/cpu-tegra3.c
+++ arch/arm/mach-tegra/cpu-tegra3.c
@@ -66,7 +66,7 @@ module_param(idle_bottom_freq, uint, 0644);
 static int mp_overhead = 10;
 module_param(mp_overhead, int, 0644);
 
-static int balance_level = 75;
+static int balance_level = 60;
 module_param(balance_level, int, 0644);
 
 static struct clk *cpu_clk;
