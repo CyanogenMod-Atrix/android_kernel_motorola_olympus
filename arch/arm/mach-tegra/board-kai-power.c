3644803bd9f0697b4df8a0bb59b01eeb85c02034
Chandler Zhang <chazhang@nvidia.com>
2012-06-15 15:57:18 +0800

    arm: tegra: fix power down sequence fps/ame
    Fix power down sequence to align with POR. FPS, GPIO and
    AME part.
    
    LDO4 tracking is not disabled.
    
    Affcted regs:
    
    FPS_SD3
    FPS_SD2
    FPS_SD1
    FPS_L4
    FPS_L3
    AME_GPIO
    GPIO3
    GPIO4
    CNFG1_L4
    CNFG1SD0
    CNFG1SD1
    
    Bug 1001267
    
    Change-Id: I8db160bf00cbe8f215c6e1b762d994d26d82809f
    Signed-off-by: Chandler Zhang <chazhang@nvidia.com>
    Reviewed-on: http://git-master/r/109134
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Jinyoung Park <jinyoungp@nvidia.com>
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Sachin Nikam <snikam@nvidia.com>

--- arch/arm/mach-tegra/board-kai-power.c
+++ arch/arm/mach-tegra/board-kai-power.c
@@ -177,16 +177,16 @@ static struct max77663_regulator_fps_cfg max77663_fps_cfgs[] = {
 	}
 
 MAX77663_PDATA_INIT(sd0,  600000, 3387500, NULL, 1, 0, 0,
-		    0, 0, -1, FPS_SRC_NONE, -1, -1, EN2_CTRL_SD0 | SD_FSRADE_DISABLE);
+		    0, 0, -1, FPS_SRC_NONE, -1, -1, EN2_CTRL_SD0);
 
 MAX77663_PDATA_INIT(sd1,  800000, 1587500, NULL, 1, 0, 0,
-		    1, 1, -1, FPS_SRC_1, FPS_POWER_PERIOD_0, -1, SD_FSRADE_DISABLE);
+		    1, 1, -1, FPS_SRC_1, FPS_POWER_PERIOD_1, FPS_POWER_PERIOD_6, 0);
 
 MAX77663_PDATA_INIT(sd2,  1800000, 1800000, NULL, 1, 0, 0,
-		    1, 1, -1, FPS_SRC_NONE, -1, -1, 0);
+		    1, 1, -1, FPS_SRC_0, -1, -1, 0);
 
 MAX77663_PDATA_INIT(sd3,  600000, 3387500, NULL, 1, 0, 0,
-		    1, 1, -1, FPS_SRC_NONE, -1, -1, 0);
+		    1, 1, -1, FPS_SRC_0, -1, -1, 0);
 
 MAX77663_PDATA_INIT(ldo0, 800000, 2350000, max77663_rails(sd3), 1, 0, 0,
 		    1, 1, -1, FPS_SRC_1, -1, -1, 0);
@@ -198,10 +198,10 @@ MAX77663_PDATA_INIT(ldo2, 800000, 3950000, NULL, 1, 0, 0,
 		    1, 1, -1, FPS_SRC_1, -1, -1, 0);
 
 MAX77663_PDATA_INIT(ldo3, 800000, 3950000, NULL, 1, 0, 0,
-		    1, 1, -1, FPS_SRC_NONE, -1, -1, 0);
+		    1, 1, -1, FPS_SRC_1, -1, -1, 0);
 
 MAX77663_PDATA_INIT(ldo4, 800000, 1587500, NULL, 0, 0, 0,
-		    1, 1, 1000000, FPS_SRC_NONE, -1, -1, LDO4_EN_TRACKING);
+		    1, 1, 1000000, FPS_SRC_0, -1, -1, LDO4_EN_TRACKING);
 
 MAX77663_PDATA_INIT(ldo5, 800000, 2800000, NULL, 0, 0, 0,
 		    1, 1, -1, FPS_SRC_NONE, -1, -1, 0);
@@ -271,15 +271,15 @@ static struct max77663_gpio_config max77663_gpio_cfgs[] = {
 	{
 		.gpio = MAX77663_GPIO3,
 		.dir = GPIO_DIR_OUT,
-		.dout = GPIO_DOUT_HIGH,
+		.dout = GPIO_DOUT_LOW,
 		.out_drv = GPIO_OUT_DRV_OPEN_DRAIN,
-		.alternate = GPIO_ALT_DISABLE,
+		.alternate = GPIO_ALT_ENABLE,
 	},
 	{
 		.gpio = MAX77663_GPIO4,
 		.dir = GPIO_DIR_OUT,
-		.dout = GPIO_DOUT_HIGH,
-		.out_drv = GPIO_OUT_DRV_PUSH_PULL,
+		.dout = GPIO_DOUT_LOW,
+		.out_drv = GPIO_OUT_DRV_OPEN_DRAIN,
 		.alternate = GPIO_ALT_ENABLE,
 	},
 	{
