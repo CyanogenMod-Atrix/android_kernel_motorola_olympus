9606d1527d43699428afe79f5e5e31e5f24bf486
mmontuori <michael.montuori@gmail.com>
2013-03-29 09:45:27 -0400

    cleaned up VNI

--- drivers/misc/vni/vni_drv.h
+++ drivers/misc/vni/vni_drv.h
@@ -34,4 +34,53 @@ struct vni_userinfo {
 
 extern struct neigh_ops arp_broken_ops;
 
+extern struct net_device *vni_dev;
+
+/* --------------------------------------------------------------------------
+ * open and close
+ */
+int vni_open(struct net_device *dev);
+
+int vni_close(struct net_device *dev);
+
+/* --------------------------------------------------------------------------
+ * get_stats: return a pointer to the device statistics
+ */
+struct net_device_stats *vni_get_stats(struct net_device *dev);
+
+/* --------------------------------------------------------------------------
+ * neighbors set up: needed for ARP to work
+ */
+int vni_neigh_setup(struct neighbour *n);
+
+int vni_neigh_setup_dev(struct net_device *dev, struct neigh_parms *p);
+
+/* --------------------------------------------------------------------------
+ * xmit: actual delivery of the data packets
+ */
+int vni_xmit(struct sk_buff *skb, struct net_device *dev);
+
+/* --------------------------------------------------------------------------
+ * ioctl: let user programs configure this interface
+ */
+int vni_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd);
+
+/* --------------------------------------------------------------------------
+ * The initialization function: it is used to assign fields in the structure
+ * Initializes the operation table and also the slave device to transmit the data 
+ */
+void vni_init(struct net_device *dev);
+
+/* --------------------------------------------------------------------------
+ * module entry point
+ */
+
+int __init vni_init_module(void);
+
+/* --------------------------------------------------------------------------
+ * module exit point
+ */
+
+void __exit vni_exit_module(void);
+
 #endif /* __VNI_DRV_H__ */
