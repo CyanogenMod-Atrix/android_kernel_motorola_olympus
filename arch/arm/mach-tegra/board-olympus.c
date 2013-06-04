487c1714d462b2b267f62cd5450cb721c1b3f107
clemsyn <clemsyn@yahoo.com>
2013-06-04 18:55:33 +0000

    tegra_gpio which most likely for battery

--- arch/arm/mach-tegra/board-olympus.c
+++ arch/arm/mach-tegra/board-olympus.c
@@ -276,7 +276,7 @@ static char oly_unused_pins_p1[] = {
         TEGRA_GPIO_PT6,
         TEGRA_GPIO_PC7,
         TEGRA_GPIO_PV7,
-        TEGRA_GPIO_PD1,
+        TEGRA_GPIO_PD1, //battery charging gpio
 };
 
 static __initdata struct tegra_clk_init_table olympus_clk_init_table[] = {
