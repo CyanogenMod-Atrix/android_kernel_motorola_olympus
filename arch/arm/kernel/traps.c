b284e5c59b40a314b674ff43312b6fba8a6a2cce
Kostas K <mpokwsths@gmail.com>
2013-09-30 23:06:32 +0300

    ARM: cacheflush: don't bother rounding to nearest vma
    do_cache_op finds the lowest VMA contained in the specified address
    range and rounds the range to cover only the mapped addresses.
    
    Since commit 4542b6a0fa6b ("ARM: 7365/1: drop unused parameter from
    flush_cache_user_range") the VMA is not used for anything else in this
    code and seeing as the low-level cache flushing routines return -EFAULT
    if the address is not valid, there is no need for this range truncation.
    
    This patch removes the VMA handling code from the cacheflushing syscall.
    
    Signed-off-by: Will Deacon <will.deacon at arm.com>

--- arch/arm/kernel/traps.c
+++ arch/arm/kernel/traps.c
@@ -453,25 +453,9 @@ static int bad_syscall(int n, struct pt_regs *regs)
 static inline void
 do_cache_op(unsigned long start, unsigned long end, int flags)
 {
-	struct mm_struct *mm = current->active_mm;
-	struct vm_area_struct *vma;
-
 	if (end < start || flags)
 		return;
-
-	down_read(&mm->mmap_sem);
-	vma = find_vma(mm, start);
-	if (vma && vma->vm_start < end) {
-		if (start < vma->vm_start)
-			start = vma->vm_start;
-		if (end > vma->vm_end)
-			end = vma->vm_end;
-
-		up_read(&mm->mmap_sem);
-		flush_cache_user_range(start, end);
-		return;
-	}
-	up_read(&mm->mmap_sem);
+	return flush_cache_user_range(start, end);
 }
 
 static inline int
