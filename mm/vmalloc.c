f945a9d635ca549a1b06deba203e1c45d9808072
Kostas K <mpokwsths@gmail.com>
2013-10-01 14:27:38 +0300

    mm/vmalloc: use wrapper function get_vm_area_size to caculate size of vm area
    Use wrapper function get_vm_area_size to calculate size of vm area.
    
    Signed-off-by: Wanpeng Li <liwanp@linux.vnet.ibm.com>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -1236,7 +1236,7 @@ void unmap_kernel_range(unsigned long addr, unsigned long size)
 int map_vm_area(struct vm_struct *area, pgprot_t prot, struct page ***pages)
 {
 	unsigned long addr = (unsigned long)area->addr;
-	unsigned long end = addr + area->size - PAGE_SIZE;
+	unsigned long end = addr + get_vm_area_size(area);
 	int err;
 
 	err = vmap_page_range(addr, end, prot, *pages);
@@ -1551,7 +1551,7 @@ static void *__vmalloc_area_node(struct vm_struct *area, gfp_t gfp_mask,
 	unsigned int nr_pages, array_size, i;
 	gfp_t nested_gfp = (gfp_mask & GFP_RECLAIM_MASK) | __GFP_ZERO;
 
-	nr_pages = (area->size - PAGE_SIZE) >> PAGE_SHIFT;
+	nr_pages = get_vm_area_size(area) >> PAGE_SHIFT;
 	array_size = (nr_pages * sizeof(struct page *));
 
 	area->nr_pages = nr_pages;
@@ -1969,7 +1969,7 @@ long vread(char *buf, char *addr, unsigned long count)
 	read_lock(&vmlist_lock);
 	for (tmp = vmlist; count && tmp; tmp = tmp->next) {
 		vaddr = (char *) tmp->addr;
-		if (addr >= vaddr + tmp->size - PAGE_SIZE)
+		if (addr >= vaddr + get_vm_area_size(tmp)
 			continue;
 		while (addr < vaddr) {
 			if (count == 0)
@@ -1979,7 +1979,7 @@ long vread(char *buf, char *addr, unsigned long count)
 			addr++;
 			count--;
 		}
-		n = vaddr + tmp->size - PAGE_SIZE - addr;
+		n = vaddr + get_vm_area_size(tmp) - addr;
 		if (n > count)
 			n = count;
 		if (!(tmp->flags & VM_IOREMAP))
@@ -2045,7 +2045,7 @@ long vwrite(char *buf, char *addr, unsigned long count)
 	read_lock(&vmlist_lock);
 	for (tmp = vmlist; count && tmp; tmp = tmp->next) {
 		vaddr = (char *) tmp->addr;
-		if (addr >= vaddr + tmp->size - PAGE_SIZE)
+		if (addr >= vaddr + get_vm_area_size(tmp))
 			continue;
 		while (addr < vaddr) {
 			if (count == 0)
@@ -2054,7 +2054,7 @@ long vwrite(char *buf, char *addr, unsigned long count)
 			addr++;
 			count--;
 		}
-		n = vaddr + tmp->size - PAGE_SIZE - addr;
+		n = vaddr + get_vm_area_size(tmp) - addr;
 		if (n > count)
 			n = count;
 		if (!(tmp->flags & VM_IOREMAP)) {
