ee6d119230095172866a28758933213aefcfa877
KrystianP <szeejk@gmail.com>
2014-08-22 23:53:30 +0200

    set sbc1 clock to auto

--- arch/arm/mach-tegra/board-olympus-clocks.c
+++ arch/arm/mach-tegra/board-olympus-clocks.c
@@ -21,7 +21,9 @@
 
 static __initdata struct tegra_clk_init_table olympus_clk_init_table[] = {
 			/* name			parent			rate		enabled */
-			{"sbc1",		"pll_c",		60000000,	true},
+		//	{"sbc1",		"pll_c",		60000000,	true},
+		//	{ "sbc1",		"pll_m",		26000000,	true},
+			{"sbc1",		NULL,			0,			true},
 		    {"sclk",		"pll_p_out4",	108000000,	true},
 			{"sbc2",		"pll_c",		60000000,	true},
 			{"pwm",			"clk_32k",		32768,		false},
