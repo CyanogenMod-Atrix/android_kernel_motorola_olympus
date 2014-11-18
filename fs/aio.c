d36c23282c6d3d76985d6f72ba3548a9798477bb
Alexandre Pereira Nunes <alexandre.nunes@gmail.com>
2014-11-18 18:08:39 -0200

    Just for consistent sake, revert this as it was superceeded by the more generic d12c11cc8d56af4da644a390d1d42e70252f0571
    Revert "I/O plug weights too much on Android (which makes heavy use of Async I/O) compared to sdhci's internal buffering."
    
    This reverts commit 6e9b02438b36ec45bf3ec90d95c63c9e934b7f68.

--- fs/aio.c
+++ fs/aio.c
@@ -1622,6 +1622,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 	struct kioctx *ctx;
 	long ret = 0;
 	int i;
+	struct blk_plug plug;
 
 	if (unlikely(nr < 0))
 		return -EINVAL;
@@ -1637,6 +1638,8 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		pr_debug("EINVAL: io_submit: invalid context id\n");
 		return -EINVAL;
 	}
+
+	blk_start_plug(&plug);
 
 	/*
 	 * AKPM: should this return a partial result if some of the IOs were
@@ -1660,6 +1663,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		if (ret)
 			break;
 	}
+	blk_finish_plug(&plug);
 
 	put_ioctx(ctx);
 	return i ? i : ret;
